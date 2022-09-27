3d035d97689b840452e33ef23dec370ecd528fef
Dario Turin <dturin1997@gmail.com>
2022-09-27 14:44:40 -0500

    Agregar archivos de proyecto.

---
+++ AMM_Lab_S5/AMM_Lab_S5/EntryDemo.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AMM_Lab_S5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EntryDemo : ContentPage
+    {
+        public EntryDemo()
+        {
+            InitializeComponent();
+        }
+    }
+}
